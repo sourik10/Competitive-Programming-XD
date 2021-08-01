4c3dbef54e22f4907e6b214e61ef4597f74cce66
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-08-01 21:54:51 +0530

    Create pattern_halfpyramid.cpp

---
+++ pattern_halfpyramid.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+	int r,c;
+	cin>>r>>c;
+	for(int i=1;i<=r;i++){
+		for(int j=1;j<=i;j++){
+			cout<<"*";
+		}
+			
+		cout<<endl;
+	}
+
+
+}
