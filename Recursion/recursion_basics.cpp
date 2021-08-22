1d02a772a6fce1bda03b391dfe616d339c498aa0
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-08-22 20:58:32 +0530

    Update recursion_basics.cpp

--- Recursion/recursion_basics.cpp
+++ Recursion/recursion_basics.cpp
@@ -27,31 +27,3 @@ int fib(int n){
 }
 
 
-//power function
-int power(int a,int n){
-	//base case
-	if(n==0){
-		return 1;
-	}
-	return a * power(a,n-1);
-}
-
-
-//power function ---faster
-int fastPower(int a,int n){
-	if(n==0){
-		return 1;
-	}
-
-	int subProb = fastPower(a,n/2);
-	int subProbSq = subProb*subProb;
-	if(n&1){
-		return a*subProbSq;
-	}
-	return subProbSq;
-}
-
-
-
-
-
