7b5cc3e64152db6cec5e17f8394b440646866491
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-08-28 23:00:18 +0530

    Create class_object.cpp

---
+++ OOPS/class_object.cpp
@@ -0,0 +1,135 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class dept_cse{
+
+private:
+	int roll;
+	char name[10];
+	int marks_eng;
+	int marks_beng;
+
+
+
+public:
+
+
+	
+//setter function()
+	void set_roll(int r){
+		roll=r;
+	}
+
+	void set_eng(int eng){
+		marks_eng=eng;
+	}
+
+
+//getter function()
+	int get_roll(){
+		return roll;
+	}
+
+	int get_eng(){
+		return marks_eng;
+	}
+
+
+
+	//default constructor 
+	dept_cse(){
+
+		cout<<"default constructor"<<endl;
+		cout<<"---------------"<<endl;
+
+	}
+
+
+	//parameterised constructor
+	dept_cse(int roll,char *n,int marks_eng,int marks_beng){
+
+		cout<<"parameterised constructor"<<endl;
+		
+
+		this->roll=roll;
+		this->marks_eng=marks_eng;
+		this->marks_beng=marks_beng;
+
+		strcpy(name,n);
+	}
+
+
+	//copy constructor
+	dept_cse(dept_cse &x){
+
+		cout<<"copy constructor"<<endl;
+
+		roll=x.roll;
+		strcpy(name,x.name);
+		marks_eng=x.marks_eng;
+		marks_beng=x.marks_beng;
+		
+		
+	}
+
+
+	void showdetails(){
+		cout<<"roll: "<<roll<<endl;
+		cout<<"name: "<<name<<endl;
+		cout<<"marks_english: "<<marks_eng<<endl;
+		cout<<"marks_bengali: "<<marks_beng<<endl;
+		
+		cout<<"-------------"<<endl;
+
+	}
+
+
+
+
+};
+
+
+int main(){
+
+
+	
+	//parameterised constructor
+	dept_cse obj1(30,"ramesh",80,90);
+	obj1.showdetails();
+
+
+
+	//defaultconstructor
+	dept_cse obj2;
+	
+
+
+
+	//copy constructor
+	dept_cse obj3(obj1);
+	obj3.showdetails();
+
+
+
+
+	/*
+//private data members(roll ,eng marks) accesed using setter and getter function
+	dept_cse obj;
+	obj.set_roll(20);
+	obj.set_eng(95);
+
+	
+
+	cout<<"roll: "<<obj.get_roll()<<endl;
+	cout<<"eng marks: "<<obj.get_eng()<<endl;
+
+	
+
+*/
+
+
+
+
+}
+
+
