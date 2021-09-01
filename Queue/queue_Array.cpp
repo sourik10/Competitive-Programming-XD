108ae4804c6c7a9e2f89bd5fa3e8c9f8253726ba
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-02 00:18:18 +0530

    Create queue_Array.cpp

---
+++ Queue/queue_Array.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include "queue_array.h"
+using namespace std;
+
+
+
+int main(){
+
+	Queue myqueue(7);
+
+	myqueue.push(1);
+	myqueue.push(2);
+	myqueue.push(3);
+	myqueue.push(4);
+	myqueue.push(5);
+	myqueue.push(6);
+	myqueue.push(7);
+	myqueue.push(8);
+	myqueue.pop();
+	myqueue.pop();
+	myqueue.push(9);
+
+
+//printing queue
+	while(!myqueue.empty()){
+		cout << myqueue.getFront() <<endl;
+		myqueue.pop();
+	}
+
+
+	return 0;
+}
