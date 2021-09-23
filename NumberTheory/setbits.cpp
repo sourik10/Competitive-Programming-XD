62b7f8feefe6c8f356a52bbd03250986e16d65d2
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-23 19:44:44 +0530

    Update setbits.cpp

--- NumberTheory/setbits.cpp
+++ NumberTheory/setbits.cpp
@@ -9,9 +9,41 @@ void setbits(int n){
 	cout<<"number of set bits in "<<temp<<" is "<<ct<<endl;
 }
 
+//alternative method
+int Solution::numSetBits(unsigned int A) {
+    //counting only numSetBits
+    unsigned int count=0;
+    while(A!=0){
+      A=A&(A-1);
+      count++; 
+    }
+    return count;
+}
 
+//no of trailing zeroes  10100 -->trailing zero= 2
+int Solution::solve(int A) {
+    int i;
+    for(i=0;i<32;i++){
+        if(A>>i & 1) break;
+    }
+    return i;
+}
 
 
 //count the number of set bits
 	cout<<__builtin_popcount(n)<<endl;
 	cout<<endl;
+
+
+//revreseBits---32bits
+unsigned int Solution::reverse(unsigned int A) {
+    unsigned int B=0;
+    for(int i=0;i<32;i++){
+        int rightBit=A & 1;   //extracting the rightmost bit
+        B=B<<1;     
+        B=B | rightBit;
+        A=A>>1;
+    }
+    return B;
+}
+
