01f56a3eac9c345156493cbbff7b0dda40d18b10
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-24 19:41:39 +0530

    Update decimal2binary.cpp

--- decimal2binary.cpp
+++ decimal2binary.cpp
@@ -5,3 +5,17 @@ void printBinary(int n){
 	}
 	cout<<endl;
 }
+
+//binary representation from interger to string
+string Solution::findDigitsInBinary(int A) {
+
+    string s;
+    if(A==0) return "0";
+    while(A>0){
+        int rem=A%2;
+        s.push_back((char)('0'+rem)); //integer to character conversion
+        A=A/2;
+    }
+    reverse(s.begin(),s.end());
+    return s;
+}
