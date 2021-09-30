8750a14a4ad2c2b6a87fac58024b1a0a71799384
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-30 23:32:49 +0530

    Create Bit++.cpp

---
+++ codeforces/Bit++.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int n; cin>>n;
+   int x=0;
+   string s;
+   while(n--){
+       cin>>s;
+       if(s=="X++") x++;
+       else if(s=="++X") ++x;
+       else if(s=="--X") --x;
+       else x--;
+       
+   }
+   cout<<x<<endl;
+   
+    
+}
